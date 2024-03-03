c60c3aef0a23fa0c723c0228c2e1d01156536a9c
Madival-Biradar <madivalbiradar14@gmail.com>
2024-03-03 16:48:35 +0530

    Fix: Fixing start.sh

--- start.sh
+++ start.sh
@@ -1,5 +1,13 @@
 #!/bin/bash
-cd."${dirname."$0"}"
+
+# Change directory to the directory of the script
+cd "$(dirname "$0")"
+
+# Activate the virtual environment (assuming it exists in fast_env/bin/activate)
 source fast_env/bin/activate
+
+# Change to the directory where your app is located (adjust if necessary)
 cd distance_fast_api
-uvicorn'main:app'--host:0.0.0.0'--workers=4
+
+# Run uvicorn with correct syntax
+uvicorn main:app --host 0.0.0.0 --workers 4
